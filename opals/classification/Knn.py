0ec3f817a8282255f39f2c7f9aeb06ffc1ba5378
Trevor Goodyear <trevor.goodyear@gtri.gatech.edu>
2017-02-08 14:38:39 -0500

    Restructure for pip installation. Change KKN to KNN

--- opals/classification/Knn.py
+++ opals/classification/Knn.py
@@ -1,4 +1,5 @@
-#Copyright (c) 2015, Georgia Tech Research Institute
+#****************************************************************
+# Copyright (c) 2015, Georgia Tech Research Institute
 # All rights reserved.
 #
 # This unpublished material is the property of the Georgia Tech
@@ -9,35 +10,37 @@
 # permission of the Georgia Tech Research Institute.
 #****************************************************************/
 
-from analytics.utils import Algorithm 
-from sklearn import svm
+from bedrock.analytics.utils import * 
+
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import scipy as sp
 import cPickle as pickle
 
-
-class Svm(Algorithm):
+class Knn(Algorithm):
     def __init__(self):
-        super(Svm, self).__init__()
+        super(Knn, self).__init__()
         self.parameters = []
         self.inputs = ['matrix.csv', 'truth_labels.csv']
         self.outputs = ['model']
-        self.name ='SVM'
+        self.name ='KNN'
         self.type = 'Classification'
-        self.description = 'Trains an SVM model using the input dataset and truth labels.'
+        self.description = 'Trains an KNN model using the input dataset with identified truth labels.'
         self.parameters_spec = []
 
     def compute(self, filepath, **kwargs):
         inputData = np.genfromtxt(filepath['matrix.csv']['rootdir'] + 'matrix.csv', delimiter=',') 
         truth = np.genfromtxt(filepath['truth_labels.csv']['rootdir'] + 'truth_labels.csv', delimiter=',')
+        model = KNeighborsClassifier()
         name = kwargs['name']
-        model = svm.SVC()
         model.fit(inputData, truth)
         modelpath = kwargs['storepath'] + name + '.p'
         pickle.dump( model, open( modelpath, "wb" ) )
         modelText = '''
 
 from analytics.utils import Algorithm 
-from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+import scipy as sp
 import numpy as np
 import cPickle as pickle
 
@@ -48,8 +51,8 @@ class %s(Algorithm):
         self.inputs = ['matrix.csv']
         self.outputs = ['assignments.csv']
         self.name ='%s'
-        self.type = 'Model'
-        self.description = 'Applies the SVM model trained on a specific dataset: %s'
+        self.type = 'Classification'
+        self.description = 'Applies the KNN model trained on a specific dataset: %s'
         self.parameters_spec = []
         self.modelfile = '%s'
 
@@ -58,12 +61,6 @@ class %s(Algorithm):
         model = pickle.load( open( self.modelfile, "rb" ) )
         assignments = model.predict(inputData)
         self.results = {'assignments.csv': assignments}
-
-    def classify(self, input):
-        model = pickle.load( open( self.modelfile, "rb" ) )
-        inputData = input.split(',')
-        return model.predict(inputData)
 ''' % (name, name, name, filepath['matrix.csv']['rootdir'], modelpath)
         self.results = {'analytic': {'text': modelText, 'classname': name} }
-
 
--- opals/classification/Svm.py
+++ opals/classification/Svm.py
@@ -1,5 +1,4 @@
-#****************************************************************
-# Copyright (c) 2015, Georgia Tech Research Institute
+#Copyright (c) 2015, Georgia Tech Research Institute
 # All rights reserved.
 #
 # This unpublished material is the property of the Georgia Tech
@@ -10,37 +9,35 @@
 # permission of the Georgia Tech Research Institute.
 #****************************************************************/
 
-from analytics.utils import * 
-
-from sklearn.neighbors import KNeighborsClassifier
+from bedrock.analytics.utils import Algorithm 
+from sklearn import svm
 import numpy as np
-import scipy as sp
 import cPickle as pickle
 
-class Kkn(Algorithm):
+
+class Svm(Algorithm):
     def __init__(self):
-        super(Kkn, self).__init__()
+        super(Svm, self).__init__()
         self.parameters = []
         self.inputs = ['matrix.csv', 'truth_labels.csv']
         self.outputs = ['model']
-        self.name ='KKN'
+        self.name ='SVM'
         self.type = 'Classification'
-        self.description = 'Trains an KKN model using the input dataset with identified truth labels.'
+        self.description = 'Trains an SVM model using the input dataset and truth labels.'
         self.parameters_spec = []
 
     def compute(self, filepath, **kwargs):
         inputData = np.genfromtxt(filepath['matrix.csv']['rootdir'] + 'matrix.csv', delimiter=',') 
         truth = np.genfromtxt(filepath['truth_labels.csv']['rootdir'] + 'truth_labels.csv', delimiter=',')
-        model = KNeighborsClassifier()
         name = kwargs['name']
+        model = svm.SVC()
         model.fit(inputData, truth)
         modelpath = kwargs['storepath'] + name + '.p'
         pickle.dump( model, open( modelpath, "wb" ) )
         modelText = '''
 
 from analytics.utils import Algorithm 
-from sklearn.neighbors import KNeighborsClassifier
-import scipy as sp
+from sklearn import svm
 import numpy as np
 import cPickle as pickle
 
@@ -51,8 +48,8 @@ class %s(Algorithm):
         self.inputs = ['matrix.csv']
         self.outputs = ['assignments.csv']
         self.name ='%s'
-        self.type = 'Classification'
-        self.description = 'Applies the KKN model trained on a specific dataset: %s'
+        self.type = 'Model'
+        self.description = 'Applies the SVM model trained on a specific dataset: %s'
         self.parameters_spec = []
         self.modelfile = '%s'
 
@@ -61,6 +58,12 @@ class %s(Algorithm):
         model = pickle.load( open( self.modelfile, "rb" ) )
         assignments = model.predict(inputData)
         self.results = {'assignments.csv': assignments}
+
+    def classify(self, input):
+        model = pickle.load( open( self.modelfile, "rb" ) )
+        inputData = input.split(',')
+        return model.predict(inputData)
 ''' % (name, name, name, filepath['matrix.csv']['rootdir'], modelpath)
         self.results = {'analytic': {'text': modelText, 'classname': name} }
+
 
